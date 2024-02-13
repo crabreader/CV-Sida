ba0c5dd656c23ab52c6a879f51d0747085961e86
Alexander Doja <alexander.doja@outlook.com>
2024-02-13 12:56:18 +0100

    broke down cv info strings to their parts

--- resources/main.js
+++ resources/main.js
@@ -22,43 +22,43 @@ async function fetchCV() {
 
 function populateJobs(obj) {
     const jobsSection = document.getElementById("arbetslivserfarenhet");
-    const jobs = obj.arbetslivserfarenhet;
+    const jobs = obj.workExperience;
 
     for (const job of jobs) {
-        const title = document.createElement("h3");
-        const descriptionI = document.createElement("i");
+        const header = document.createElement("h3");
+        const description = document.createElement("i");
         const expList = document.createElement("ul");
 
-        title.textContent = job.titel;
-        descriptionI.textContent = job.info;
+        header.textContent = job.roleName;
+        description.textContent = `${job.yearStart}-${job.yearEnd} ${job.companyName}, ${job.location}`;
 
-        for (const exp of job.erfarenheter)
+        for (const exp of job.experiences)
         {
             const listItem = document.createElement("li");
             listItem.textContent = exp;
             expList.appendChild(listItem);
         }
 
-        jobsSection.appendChild(title);
-        jobsSection.appendChild(descriptionI);
+        jobsSection.appendChild(header);
+        jobsSection.appendChild(description);
         jobsSection.appendChild(expList);
     }
 }
 
 function populateEducation(obj) {
     const eduSection = document.getElementById("utbildning");
-    const educations = obj.utbildning;
+    const educations = obj.education;
 
-    for (const edu of educations) {
-        const title = document.createElement("h3");
+    for (const education of educations) {
+        const name = document.createElement("h3");
         const info = document.createElement("i");
         const description = document.createElement("p");
 
-        title.textContent = edu.namn;
-        info.textContent = edu.info;
-        description.textContent = edu.beskrivning;
+        name.textContent = education.name;
+        info.textContent = `${education.yearStart}-${education.yearEnd} ${education.schoolName}, ${education.location}`;
+        description.textContent = education.description;
 
-        eduSection.appendChild(title);
+        eduSection.appendChild(name);
         eduSection.appendChild(info);
         eduSection.appendChild(description);
     }
