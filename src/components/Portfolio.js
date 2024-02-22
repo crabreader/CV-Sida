de895171dd63ce56d0b188cc024a54bba16f7aba
Alexander Doja <alexander.doja@outlook.com>
2024-02-22 17:19:54 +0100

    Fixed alignment error of loading text

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -22,8 +22,6 @@ const Portfolio = () => {
         fetchRepos();
     }, []);
 
-
-
     return (
         <div className='page-layout'>
             <MenuButton />
@@ -50,91 +48,5 @@ const Portfolio = () => {
         </div>
     );
 };
-
-{/*function Portfolio() {
-    return (
-        <>
-            <div className='page-layout'>
-                <MenuButton />
-                <Navbar />
-                <main>
-                    <h1>Portfolio</h1>
-                    <hr />
-                    <div class="portfolio-grid">
-                        <div class="portfolio-item">
-                            <h2>C# Övningar</h2>
-                            <hr />
-                            <div class="button-wrapper">
-                                <a class="button" href="#popup1">Visa mer</a>
-                            </div>
-                            <div id="popup1" class="overlay">
-                                <div class="popup">
-                                    <h2>C# Övningar</h2>
-                                    <a class="close" href="#">x</a>
-                                    <div class="content">
-                                        <p>Grundläggande programmering i C#.</p>
-                                        <a href="https://github.com/crabreader/NET23">Github</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio-item">
-                            <h2>Individuellt Databasprojekt</h2>
-                            <hr />
-                            <div class="button-wrapper">
-                                <a class="button" href="#popup2">Visa mer</a>
-                            </div>
-                            <div id="popup2" class="overlay">
-                                <div class="popup">
-                                    <h2>Individuellt Databasprojekt</h2>
-                                    <a class="close" href="#">x</a>
-                                    <div class="content">
-                                        <p>En databas med flera tabeller och en konsolapplikation för att interagera med databasen.</p>
-                                        <a href="https://github.com/crabreader/Individuellt-databasprojekt">Github</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio-item">
-                            <h2>Bank App</h2>
-                            <hr />
-                            <div class="button-wrapper">
-                                <a class="button" href="#popup3">Visa mer</a>
-                            </div>
-                            <div id="popup3" class="overlay">
-                                <div class="popup">
-                                    <h2>Bank App</h2>
-                                    <a class="close" href="#">x</a>
-                                    <div class="content">
-                                        <p>Jag medverkade i det här grupparbetet för att skapa ett enkelt banksystem.</p>
-                                        <a href="https://github.com/ByteBears-NET23/UntitledBankApp">Github</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio-item">
-                            <h2>CV-Sida</h2>
-                            <hr />
-                            <div class="button-wrapper">
-                                <a class="button" href="#popup4">Visa mer</a>
-                            </div>
-                            <div id="popup4" class="overlay">
-                                <div class="popup">
-                                    <h2>CV-Sida</h2>
-                                    <a class="close" href="#">x</a>
-                                    <div class="content">
-                                        <p>Den här webbsidan :)</p>
-                                        <p>Skriven i HTML, CSS och Javascript.</p>
-                                        <a href="https://github.com/crabreader/CV-Sida">Github</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-            </div>
-        </>
-    );
-}*/}
 
 export default Portfolio;
